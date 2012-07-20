90bf5d2b419e5c14c3910e20381358d0296a2fea
theofanislekkas <theofanis.lekkas@theofanislekkas.com>
2012-07-20 10:11:54 -0500

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from taggit.managers import TaggableManager
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=60)
+	body = models.TextField()
+	created = models.DateTimeField()
+	tags = TaggableManager()
+
+	def __unicode__(self):
+		return self.title
